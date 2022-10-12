e9754a8312f566f2bb61cf1dc24bee95a9deb492
Pratik Pralhad Argade <90461882+argadepp@users.noreply.github.com>
2022-10-12 12:03:33 +0530

    Update createstack.sh

--- template/script/createstack.sh
+++ template/script/createstack.sh
@@ -19,18 +19,18 @@
 
 #!/bin/bash
 
-parameterfilepath="${WORKSPACE}/template/parameters.json"
+parameterfilepath="file://${WORKSPACE}/template/parameters.json"
 
 echo "!!!!!!!!!!!!!!!!!!!!!!!Substitute the parameters!!!!!!!!!!!!!!!!"
 envsubst < "${WORKSPACE}/template/asg_parameters.json.tmpl" > "${WORKSPACE}/template/parameters.json"
-templateUrl="file://${WORKSPACE}/template/asg.yaml"
+templateUrl="${WORKSPACE}/template/asg.yaml"
 #cat "${WORKSPACE}/template/asg_parameters.json.tmpl"
 echo "!!!!!!!!!!!!!!!!Final Parameters!!!!!!!!!!!!!!!!"
 cat "${WORKSPACE}/template/parameters.json"
 
 echo "!!!!!!!!!!!!! Creating stack !!!!!!!!!!!!!!!!!!!!!!!!"
-aws cloudformation create-stack \
---template-body "${templateUrl}" --region "${aws_region}" \
+aws cloudformation "${action}"-stack \
+--template-url "${templateUrl}" --region "${aws_region}" \
 --stack-name "${stackName}" \
---parameters  "file://${WORKSPACE}/template/parameters.json" \
+--parameters  "${parameterfilepath}" \
 --capabilities CAPABILITY_NAMED_IAM

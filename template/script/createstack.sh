e6c3dbe05af62e521a397bc67676644cb38cb400
Pratik Pralhad Argade <90461882+argadepp@users.noreply.github.com>
2022-10-12 11:37:24 +0530

    Update createstack.sh

--- template/script/createstack.sh
+++ template/script/createstack.sh
@@ -12,4 +12,4 @@ export VPC
 envsubst < "${WORKSPACE}/script/asg_parameters.json.tmpl" > "${WORKSPACE}/script/parameters.json"
 
 
-cat "${WORKSOPACE}/asg_parameters.json.tmpl"
+cat "${WORKSPACE}/asg_parameters.json.tmpl"

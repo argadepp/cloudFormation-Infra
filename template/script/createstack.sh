ce1aea7c02fe128b035cfb8e05facaac17f18093
Pratik Pralhad Argade <90461882+argadepp@users.noreply.github.com>
2022-10-12 11:55:43 +0530

    Update createstack.sh

--- template/script/createstack.sh
+++ template/script/createstack.sh
@@ -1,17 +1,36 @@
+# #!/bin/bash
+
+
+# echo "${VPC}"
+
+# parameterfilepath="${WORKSPACE}/template/parameters.json"
+
+# echo VPC = "${VPC}"
+
+# export VPC
+
+# envsubst < "${WORKSPACE}/template/asg_parameters.json.tmpl" > "${WORKSPACE}/template/parameters.json"
+
+
+# cat "${WORKSPACE}/template/asg_parameters.json.tmpl"
+# echo "Final Parameters"
+# cat "${WORKSPACE}/template/parameters.json"
+
+
 #!/bin/bash
-
-
-echo "${VPC}"
 
 parameterfilepath="${WORKSPACE}/template/parameters.json"
 
-echo VPC = "${VPC}"
-
-export VPC
-
+echo "!!!!!!!!!!!!!!!!!!!!!!!Substitute the parameters!!!!!!!!!!!!!!!!"
 envsubst < "${WORKSPACE}/template/asg_parameters.json.tmpl" > "${WORKSPACE}/template/parameters.json"
-
-
-cat "${WORKSPACE}/template/asg_parameters.json.tmpl"
-echo "Final Parameters"
+templateUrl="${WORKSPACE}/template/asg.yaml"
+#cat "${WORKSPACE}/template/asg_parameters.json.tmpl"
+echo "!!!!!!!!!!!!!!!!Final Parameters!!!!!!!!!!!!!!!!"
 cat "${WORKSPACE}/template/parameters.json"
+
+echo "!!!!!!!!!!!!! Creating stack !!!!!!!!!!!!!!!!!!!!!!!!"
+aws cloudformation create-stack \
+--template-url "${templateUrl}" --region "${aws_region}" \
+--stack-name "${stackName}" \
+--parameters  "file://${WORKSPACE}/template/parameters.json" 
+--capabilities CAPABILITY_NAMED_IAM

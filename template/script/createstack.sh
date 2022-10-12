6910dbe389670794e52f95cb306deee1e2f917b6
Pratik Pralhad Argade <90461882+argadepp@users.noreply.github.com>
2022-10-12 12:20:13 +0530

    Update createstack.sh

--- template/script/createstack.sh
+++ template/script/createstack.sh
@@ -28,9 +28,16 @@ templateUrl="file://${WORKSPACE}/template/asg.yaml"
 echo "!!!!!!!!!!!!!!!!Final Parameters!!!!!!!!!!!!!!!!"
 cat "${WORKSPACE}/template/parameters.json"
 
-echo "!!!!!!!!!!!!! Creating stack !!!!!!!!!!!!!!!!!!!!!!!!"
+echo "!!!!!!!!!!!!! ${action} of ${environment}-${stackName} stack is initiated !!!!!!!!!!!!!!!!!!!!!!!!"
 aws cloudformation "${action}"-stack \
 --template-body "${templateUrl}" --region "${aws_region}" \
---stack-name "${stackName}" \
+--stack-name "${environment}-${stackName}" \
 --parameters  "${parameterfilepath}" \
 --capabilities CAPABILITY_NAMED_IAM
+
+echo "Waiting for the '${action}' operation to complete on CloudFormation stack: ${environment}-${stackName}"
+aws cloudformation wait stack-${action}-complete \
+    --stack-name ${environment}-${stackName} \
+    --region ${aws_region}
+    
+echo "${stackName} is created successfully !!!!!!!!!!!!!!!!"    
